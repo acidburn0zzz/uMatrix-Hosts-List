88dc5fe4a78cc8d5d79566f5cfa8b80871533cd5
uBlock-user <uBlock-user@users.noreply.github.com>
2017-04-07 00:14:55 +0530

    Update and rename uBO-Extra.user.js to HelloWorld.user.js

---
+++ HelloWorld.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Hello-World
+// @version      2.21
+// @author       Hello-World
+// @include      google.com
+// @grant        none
+// @run-at       document-start
+// @downloadURL  https://github.com/uBlock-user/uMatrix-Hosts-List/raw/master/HelloWorld.user.js
+// ==/UserScript==
+
+alert("Hello World");
